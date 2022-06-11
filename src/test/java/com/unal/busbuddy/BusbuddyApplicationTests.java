d7e5142008ca18974f4791e44d9cb401e1ed2c14
juzambranol <105232568+juzambranol@users.noreply.github.com>
2022-06-11 12:43:19 -0500

    Add files via upload
    spring initializer

---
+++ src/test/java/com/unal/busbuddy/BusbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unal.busbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
