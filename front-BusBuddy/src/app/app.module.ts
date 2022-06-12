e2df5065c8d75c58eada71de3d464205b49841f1
Nicolnegg <princesnegg@gmail.com>
2022-06-11 19:20:51 -0500

    homeususario creacion

--- front-BusBuddy/src/app/app.module.ts
+++ front-BusBuddy/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeUsuarioComponent } from './home-usuario/home-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeUsuarioComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-BusBuddy/src/app/home-usuario/home-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-usuario',
+  templateUrl: './home-usuario.component.html',
+  styleUrls: ['./home-usuario.component.css']
+})
+export class HomeUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
