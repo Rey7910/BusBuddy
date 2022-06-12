a549f62275185623d58cff82a8afd7974b863894
Nicolnegg <princesnegg@gmail.com>
2022-06-11 19:52:01 -0500

    Home Usuario y rutas

--- front-BusBuddy/src/app/app-routing.module.ts
+++ front-BusBuddy/src/app/app-routing.module.ts
@@ -1,17 +1,24 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeUsuarioComponent } from './home-usuario/home-usuario.component';
 
 
 const routes: Routes = [
-  { path: 'login',
-  component: LoginComponent,
+  {
+    path: 'login',
+    component: LoginComponent,
 },
 {
   path: '',
   redirectTo: 'login',
   pathMatch: 'full',
-}
+},
+{
+    path: 'home-usuario',
+    component: HomeUsuarioComponent,
+  },
+
 ];
 
 @NgModule({
