c3f6c0f0ddab9e51a42d3c3f782134792c38f0f1
Juan Camilo <juzambranol@unal.edu.co>
2022-06-13 01:28:17 -0500

    no actualiza (?)

--- src/main/java/com/unal/BusBuddyBack/repository/RutaRepository.java
+++ src/main/java/com/unal/BusBuddyBack/repository/RutaRepository.java
@@ -4,5 +4,5 @@ import com.unal.BusBuddyBack.entidades.Ruta;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RutaRepository extends CrudRepository<Ruta,Integer>{
-
+//a
 }
