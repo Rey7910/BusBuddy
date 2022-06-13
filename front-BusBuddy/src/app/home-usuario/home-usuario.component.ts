d87bb7d18f530a9005aba232348fbd3539ca015a
Juan Camilo <juzambranol@unal.edu.co>
2022-06-12 23:30:48 -0500

    aaa

--- front-BusBuddy/src/app/home-usuario/home-usuario.component.ts
+++ front-BusBuddy/src/app/home-usuario/home-usuario.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router'
+import { Ruta } from '../Modelo/Ruta';
+import {ServiceRutasService} from '../Service/service-rutas.service'
 @Component({
   selector: 'app-home-usuario',
   templateUrl: './home-usuario.component.html',
@@ -9,13 +11,22 @@ export class HomeUsuarioComponent implements OnInit {
 
   tiquetever = false;
 
-  constructor() { }
-
+  constructor(private service:ServiceRutasService, private router:Router) { }
+  rutas:Ruta[];
   ngOnInit(): void {
+    this.service.getRutas().subscribe(data=>{
+      this.rutas=data;
+    }) 
   }
 
   AparecerTiquetes(){
+    console.log('aaa');
+    this.service.getRutas().
+    subscribe(data=>{
+      this.rutas=data;
+    }) ;
     this.tiquetever = true;
   }
+
 
 }
