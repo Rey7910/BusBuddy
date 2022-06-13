cc9eb28251967a1d7f3d2332d8c858440f22e922
Juan Camilo <juzambranol@unal.edu.co>
2022-06-12 21:04:51 -0500

    repositorio

---
+++ src/main/java/com/unal/BusBuddyBack/RestControllers/ClienteRestController.java
@@ -0,0 +1,25 @@
+package com.unal.BusBuddyBack.RestControllers;
+
+import com.unal.BusBuddyBack.entidades.Ruta;
+import com.unal.BusBuddyBack.repository.RutaRepository;
+import com.unal.BusBuddyBack.services.RutaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200",maxAge = 3600)
+@RestController
+@RequestMapping()
+public class ClienteRestController {
+    @Autowired
+    private RutaService service;
+    @GetMapping()
+    public List<Ruta> consultar(){
+
+
+    };
+}
---
+++ src/main/java/com/unal/BusBuddyBack/repository/RutaRepository.java
@@ -0,0 +1,8 @@
+package com.unal.BusBuddyBack.repository;
+
+import com.unal.BusBuddyBack.entidades.Ruta;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RutaRepository extends CrudRepository<Ruta,Integer>{
+
+}
