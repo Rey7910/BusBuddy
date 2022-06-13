cb71fac8d37032165bc068f916404af8e6cf53f4
Juan Camilo <juzambranol@unal.edu.co>
2022-06-13 01:24:40 -0500

    Por fin Hp, Funcionando conexion con front, falta ultimar detalles en el manejo de clases recibidas en front, habia un error en la url

--- src/main/java/com/unal/BusBuddyBack/BusBuddyBackApplication.java
+++ src/main/java/com/unal/BusBuddyBack/BusBuddyBackApplication.java
@@ -2,12 +2,24 @@ package com.unal.BusBuddyBack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class BusBuddyBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusBuddyBackApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
+	}
 }
--- src/main/java/com/unal/BusBuddyBack/RestControllers/ClienteRestController.java
+++ src/main/java/com/unal/BusBuddyBack/RestControllers/ClienteRestController.java
@@ -2,6 +2,7 @@ package com.unal.BusBuddyBack.RestControllers;
 
 import com.unal.BusBuddyBack.entidades.Ruta;
 import com.unal.BusBuddyBack.repository.RutaRepository;
+import com.unal.BusBuddyBack.services.IRutaService;
 import com.unal.BusBuddyBack.services.RutaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,12 +14,13 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200",maxAge = 3600)
 @RestController
-@RequestMapping()
+@RequestMapping({"/rutas"})
 public class ClienteRestController {
     @Autowired
-    private RutaService service;
-    @GetMapping()
+    private IRutaService service;
+    @GetMapping
     public List<Ruta> consultar(){
+        System.out.println("AAOOAA");
         return service.listar();
-    };
+    }
 }
