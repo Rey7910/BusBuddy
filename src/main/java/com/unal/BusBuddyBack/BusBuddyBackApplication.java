f6c68786b07bdcbfc91a6fab91fa4385db1d91bc
Juan Camilo <juzambranol@unal.edu.co>
2022-06-14 19:58:20 -0500

    correccion error bean

--- src/main/java/com/unal/BusBuddyBack/BusBuddyBackApplication.java
+++ src/main/java/com/unal/BusBuddyBack/BusBuddyBackApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@ComponentScan(basePackages = {"com.unal.BusBuddyBack.BusBuddyBackApplication"}) //Soluciona problemita del bean
 public class BusBuddyBackApplication {
 
 	public static void main(String[] args) {
