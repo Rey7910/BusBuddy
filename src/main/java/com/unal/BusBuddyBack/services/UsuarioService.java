0e0fdd28dc87aa3a699ea7d8dcdec8b463cd7080
Juan Camilo <juzambranol@unal.edu.co>
2022-06-14 19:34:07 -0500

    .

--- src/main/java/com/unal/BusBuddyBack/services/UsuarioService.java
+++ src/main/java/com/unal/BusBuddyBack/services/UsuarioService.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.unal.BusBuddyBack.entidades.Usuario;
 import com.unal.BusBuddyBack.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 
 public class UsuarioService implements IUsuarioService {
 
