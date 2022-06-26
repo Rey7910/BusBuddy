12b35805c27a2d7f71cb74048e463358fa74e1c0
Juan Camilo <juzambranol@unal.edu.co>
2022-06-25 23:45:33 -0500

    editar completado 2

--- front-BusBuddy/src/app/asignar-rutas-empresa/asignar-rutas-empresa.component.ts
+++ front-BusBuddy/src/app/asignar-rutas-empresa/asignar-rutas-empresa.component.ts
@@ -5,6 +5,8 @@ import { ServiceRutasService } from '../Service/service-rutas.service';
 import { ServiceRutasViewService } from '../Service/service-rutas-view.service';
 import { RutaView } from '../Modelo/RutaView';
 import { ToastrService } from 'ngx-toastr';
+import { Terminal } from '../Modelo/Terminal';
+import { ServiceTerminalService } from '../Service/service-terminal.service';
 @Component({
   selector: 'app-asignar-rutas-empresa',
   templateUrl: './asignar-rutas-empresa.component.html',
@@ -12,14 +14,18 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class AsignarRutasEmpresaComponent implements OnInit {
   caja_editar = false;
-  constructor(private service:ServiceRutasService,private serviceView:ServiceRutasViewService,private toastr: ToastrService, private router:Router) { }
-  rutas:RutaView[]
+  constructor(private service:ServiceRutasService,private serviceView:ServiceRutasViewService,private serviceTerminal:ServiceTerminalService,private toastr: ToastrService, private router:Router) { }
+  rutas:RutaView[];
+  terminales:Terminal[];
   newRuta=new Ruta();
   ngOnInit(): void {
     this.serviceView.getRutas().
     subscribe(data=>{
       this.rutas=data;
     }) ;
+    this.serviceTerminal.getTerminales().subscribe(data=>{
+      this.terminales=data;
+    });
     sessionStorage.setItem("idEmpresa",'1');
   }
   Crear(ruta:Ruta):void{
@@ -41,11 +47,25 @@ export class AsignarRutasEmpresaComponent implements OnInit {
     }
     
   }
-  Editar(id:number):void{
-    sessionStorage.setItem("idAux",id.toString());
+  Editar(ruta:Ruta):void{
+    var idE=sessionStorage.getItem("idEmpresa");
+    if(idE!=null){
+      ruta.idEmpresa=+idE;
+      this.service.crearRuta(ruta).subscribe(data=>{
+        this.toastr.success("Ruta Editada con exito");
+        window.location.reload();
+      },err =>this.toastr.error("Verifica datos, ha ocurrido un error"));
+    this.serviceView.getRutas().
+    subscribe(data=>{
+      this.rutas=data;
+    }) ;
+    }else{
+      this.toastr.error("Ruta No Editada, error con la identificación de la empresa");
+    }
   }
 
-  Editar2(){
+  Editar2(id:number){
+    this.newRuta.idRuta=id;
     this.caja_editar = true; 
   }
 
