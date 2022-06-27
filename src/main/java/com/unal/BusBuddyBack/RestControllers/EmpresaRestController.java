baf23b1dc51b5d33f5370acef89301f34d83848a
Lanna-27 <lalacast2000@gmail.com>
2022-06-27 01:15:34 -0500

    corrección métodos Mapping

--- src/main/java/com/unal/BusBuddyBack/RestControllers/EmpresaRestController.java
+++ src/main/java/com/unal/BusBuddyBack/RestControllers/EmpresaRestController.java
@@ -1,8 +1,11 @@
 package com.unal.BusBuddyBack.RestControllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.unal.BusBuddyBack.entidades.Empresa;
 import com.unal.BusBuddyBack.services.EmpresaService;
 
 @CrossOrigin(origins = "http://localhost:4200",maxAge = 3600)
@@ -12,6 +15,9 @@ public class EmpresaRestController {
     @Autowired
     EmpresaService servEmpresa;
 
-    
-    
+    @GetMapping
+    public List<Empresa> consultar(){
+        return servEmpresa.listar();
+    }
+
 }
--- src/main/java/com/unal/BusBuddyBack/RestControllers/UsuarioRestController.java
+++ src/main/java/com/unal/BusBuddyBack/RestControllers/UsuarioRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +29,10 @@ public class UsuarioRestController {
     @PostMapping
     public Usuario agregar(@RequestBody Usuario usuario){
         return servUsuario.save(usuario);
-    }    
+    } 
+    
+    @PutMapping
+    public Usuario editar(@RequestBody Usuario usuario){
+        return servUsuario.save(usuario);
+    }
 }
