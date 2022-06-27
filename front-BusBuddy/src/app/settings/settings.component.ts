c7836696b4fdb1ef99ed7472a17da596b345a711
AngelSilva2002 <apenarredonda@unal.edu.co>
2022-06-26 23:37:55 -0500

    Settings

--- front-BusBuddy/src/app/settings/settings.component.ts
+++ front-BusBuddy/src/app/settings/settings.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  caja_info = true;
+  caja_pass = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ver_info(){
+    this.caja_info = true;
+    this.caja_pass = false;
+  }
+  ver_pass(){
+    this.caja_info = false;
+    this.caja_pass = true;
   }
 
 }
