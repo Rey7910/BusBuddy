5f0a1ae27510f568b9725c5499ee4aa0b9cea695
Juan Camilo <juzambranol@unal.edu.co>
2022-06-26 23:19:39 -0500

    Enlace con Back, mis-tiquetes

--- front-BusBuddy/src/app/mis-tiquetes-usuario/mis-tiquetes-usuario.component.ts
+++ front-BusBuddy/src/app/mis-tiquetes-usuario/mis-tiquetes-usuario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Reserva } from '../Modelo/Reserva';
+import { ReservaView } from '../Modelo/ReservaView';
+import { ServiceReservasViewService } from '../Service/service-reservas-view.service';
 
 @Component({
   selector: 'app-mis-tiquetes-usuario',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mis-tiquetes-usuario.component.css']
 })
 export class MisTiquetesUsuarioComponent implements OnInit {
-
-  constructor() { }
-
+  tiquetes:ReservaView[];
+  constructor(private serviceReservasView:ServiceReservasViewService, private router:Router) { }
+  
   ngOnInit(): void {
+    sessionStorage.setItem("idUsuario",'1');
+    this.filtrarTodos();
   }
-
+  filtrarTodos(){
+    var idE=sessionStorage.getItem("idUsuario");
+    if(idE!=null){
+      var id: number= +idE;
+      this.serviceReservasView.getReservasUsuario(id).subscribe(data=> //0 Para validar boletos aun no validados
+        this.tiquetes=data
+      );
+    }
+    
+  }
+  filtrar(estado:number){
+    var idE=sessionStorage.getItem("idEmpresa");
+    if(idE!=null){
+      var id: number= +idE;
+      this.serviceReservasView.getReservasUsuarioYEstado(id,estado).subscribe(data=> //0 Para validar boletos aun no validados
+        this.tiquetes=data
+      );
+    }
+  }
+  filtrarVencidos(){
+    this.filtrar(2);
+  }
+  filtrarDisponibles(){
+    this.filtrar(0);
+  }
+  filtrarAbordados(){
+    this.filtrar(1);
+  }
 }
