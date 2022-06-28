bf833b26d368a8f5fecdbc11e47b8e6ea3ce5c4c
Lanna-27 <lalacast2000@gmail.com>
2022-06-28 00:04:51 -0500

    corrección adminEmpresa

--- src/main/java/com/unal/BusBuddyBack/services/AdminEmpresaService.java
+++ src/main/java/com/unal/BusBuddyBack/services/AdminEmpresaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.unal.BusBuddyBack.entidades.AdministradorEmpresa;
 import com.unal.BusBuddyBack.repository.AdminEmpresaRepository;
 
+@Service
 public class AdminEmpresaService implements IAdminEmpresaService {
 
     @Autowired
