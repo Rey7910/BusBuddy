98d6da85bc37afad19d38524237662c0226100ce
Rey7910 <70295723+Rey7910@users.noreply.github.com>
2022-06-27 23:32:36 -0500

    Create inserts.sql

---
+++ src/Data/DataBase/inserts.sql
@@ -0,0 +1,23 @@
+-- Terminales
+
+insert into terminal(idalterno,nombre,ciudad,capacidad,estado) values (2,'Salitre','Bogotá',300,1);
+insert into terminal(idalterno,nombre,ciudad,capacidad,estado) values (1,'Satélite del Norte','Bogotá',300,1);
+insert into terminal(idalterno,nombre,ciudad,capacidad,estado) values (4,'Terminal de Tibasosa','Tibasosa',300,1);
+insert into terminal(idalterno,nombre,ciudad,capacidad,estado) values (3,'Terminal de Duitama','Sogamoso',300,1);
+insert into terminal(idalterno,nombre,ciudad,capacidad,estado) values (6,'Terminal El Tambo','El Tambo',300,1);
+insert into terminal(idalterno,nombre,ciudad,capacidad,estado) values (5,'Terminal de Pasto','Pasto',300,1);
+insert into terminal(idalterno,nombre,ciudad,capacidad,estado) values (8,'Estación de Tame','Tame',100,1);
+insert into terminal(idalterno,nombre,ciudad,capacidad,estado) values (7,'Terminal de Arauca','Arauca',300,1);
+insert into terminal(idalterno,nombre,ciudad,capacidad,estado) values (10,'Terminal de transportes de Barrancabermeja','Barrancabermeja',300,1);
+insert into terminal(idalterno,nombre,ciudad,capacidad,estado) values (9,'Terminal de Bucaramanga','Bucaramanga',300,1);
+insert into terminal(idalterno,nombre,ciudad,capacidad,estado) values (12,'Terminal del Norte','Medellín',300,1);
+insert into terminal(idalterno,nombre,ciudad,capacidad,estado) values (11,'Estación de Envigado','Envigado',100,1);
+
+
+-- Empresas
+
+insert into empresa(nombre,nit,email) values ('Copetrinas','890.215.624','soporte@copetrinas.com');
+insert into empresa(nombre,nit,email) values ('Pelicano','856.267.754','soporte@pelicano.com');
+insert into empresa(nombre,nit,email) values ('Del norte','874.522.758','soporte@delnorte.com');
+insert into empresa(nombre,nit,email) values ('Cigarras','824.452.325','soporte@cigarras.com');
+insert into empresa(nombre,nit,email) values ('Conquistadores','860.322.145','soporte@cigarras.com');
