cbde26961fe568388a9d66b4a7d784e08b5c0fe7
Juan Camilo <juzambranol@unal.edu.co>
2022-06-29 14:44:41 -0500

    Conexión servicio estadisticas

--- front-BusBuddy/src/app/Service/service-estadisticas.service.ts
+++ front-BusBuddy/src/app/Service/service-estadisticas.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ContadoresView } from '../Modelo/ContadoresView';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceEstadisticasService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  Url='http://localhost:8090/estadisticas';
+
+  getContadores(){
+    return this.http.get<ContadoresView[]>(this.Url+"contadores_estadoRutas");
+  }
+
 }
